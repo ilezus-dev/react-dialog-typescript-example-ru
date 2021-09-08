a728ffe7fd077a82c3ac072f3c29048e3e9f5a14
Ilez Kurskiev <ilez.kurskiev@gmail.com>
2021-09-08 19:30:07 +0300

    Some fixes

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 
 import Dialog from "./components/Dialog";
 import Sender from "./components/Sender";
